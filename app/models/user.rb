122eb1af2559794d74165dfa83efc1871a80945d
Shoumik Chowdhury <shoumikchowdhury@Shoumiks-Air.hitronhub.home>
2022-02-04 16:21:38 -0600

    add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
 
+  VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+
+  validates :name, presence: true
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 6 }
 end
