669e0dbc5d81b4187ab5b6877cb036a24c598cf3
Shoumik Chowdhury <shoumikchowdhury@Shoumiks-Air.hitronhub.home>
2022-02-04 20:35:18 -0600

    add create and destroy actions.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,16 @@
 class SessionsController < ApplicationController
+  def create
+    user = User.find_by(email: params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+      session[:current_user_id] = user.id
+      render plain: "logged in as #{user.name}"
+    else
+      render plain: "cannot login"
+    end
+  end
+
+  def destroy
+    session[:current_user_id] = nil
+    render plain: "logged out"
+  end
 end
