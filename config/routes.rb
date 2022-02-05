a3487b69e3d07a0bd04ca1b5464c6c372dc2e227
Shoumik Chowdhury <shoumikchowdhury@Shoumiks-Air.hitronhub.home>
2022-02-04 20:37:10 -0600

    add /login, /logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   resources :users, only: [:create]
+
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
 end
