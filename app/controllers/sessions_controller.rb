ea6e27a112a6f5ba88381cf484339a0d01f5f28f
Shoumik Chowdhury <shoumikchowdhury@Shoumiks-Air.hitronhub.home>
2022-02-05 01:19:53 -0600

    email changed to lowercase, whitespace removed before db query. Status code added to renders

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,17 @@
 class SessionsController < ApplicationController
   def create
-    user = User.find_by(email: params[:session][:email])
+    # finds user by email by changing input to lowercase and removing trailing whitespace
+    user = User.find_by(email: params[:session][:email].downcase.strip)
     if user && user.authenticate(params[:session][:password])
       session[:current_user_id] = user.id
-      render plain: "logged in as #{user.name}"
+      render plain: "logged in as #{user.name}", status: :ok
     else
-      render plain: "cannot login"
+      render plain: "cannot login", status: :bad_request
     end
   end
 
   def destroy
     session[:current_user_id] = nil
-    render plain: "logged out"
+    render plain: "logged out", status: :ok
   end
 end
